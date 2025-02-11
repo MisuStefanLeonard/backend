ede96224f5ad1405b64c55e2e42d8f8ef2faa297
MisuStefanLeonard <aade211@yahoo.com>
2025-02-11 17:26:33 +0200

    Update Program.cs

--- C# projects/E_Commerce_BackEnd/E_Commerce_BackEnd/Program.cs
+++ C# projects/E_Commerce_BackEnd/E_Commerce_BackEnd/Program.cs
@@ -84,7 +84,7 @@ builder.Services.AddSingleton<IAmazonKeyManagementService>
 // user-secrets
 
 var credentials = FallbackCredentialsFactory.GetCredentials();
-Console.WriteLine($"Using AWS Credentials: {credentials.GetCredentials().AccessKey}");
+Console.WriteLine($"Using AWS Credentials: {credentials.GetCredentials().AccountId}");
 builder.Configuration.AddUserSecrets<Program>();
 // Quartz integration for task scheduling
 
