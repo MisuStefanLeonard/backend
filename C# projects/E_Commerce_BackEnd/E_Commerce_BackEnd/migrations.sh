247497e8d31aa139547def1bcabc6f1b4abefcd4
MisuStefanLeonard <aade211@yahoo.com>
2025-02-18 17:34:10 +0200

    Update migrations.sh

--- C# projects/E_Commerce_BackEnd/E_Commerce_BackEnd/migrations.sh
+++ C# projects/E_Commerce_BackEnd/E_Commerce_BackEnd/migrations.sh
@@ -36,7 +36,7 @@ echo "🚀 Running database migrations..."
 
 dotnet tool list --global || { echo "❌ dotnet-ef is missing!"; exit 1; }
 
-export PATH="$PATH:/root/.dotnet/tools"
+#export PATH="$PATH:/root/.dotnet/tools"
 dotnet ef database update --no-build 
 
 echo "✅ Migrations applied successfully!"
