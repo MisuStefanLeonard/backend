767571ad9d9d4edb06064881b091299e0cd2d170
MisuStefanLeonard <aade211@yahoo.com>
2025-02-18 13:17:25 +0200

    update

--- C# projects/E_Commerce_BackEnd/E_Commerce_BackEnd/general.sh
+++ C# projects/E_Commerce_BackEnd/E_Commerce_BackEnd/general.sh
@@ -2,10 +2,17 @@
 
 set -e  # Exit on any error
 
+echo "Check is migrations.sh exists..."
+ls -l /src/migrations.sh ||  { echo "❌ migrations.sh NOT FOUND!"; exit 1; }
+
 echo "Running database migrations..."
 /bin/bash /src/migrations.sh
+
+echo "Check is quartzInit.sh exists..."
+ls -l /src/quartzInit.sh ||  { echo "❌ quartzInit.sh NOT FOUND!"; exit 1; }
 
 echo "Running Quartz initialization..."
 /bin/bash /src/quartzInit.sh
 
 echo "Starting the application..."
+exit 0;
