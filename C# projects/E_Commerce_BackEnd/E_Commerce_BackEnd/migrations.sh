362661a2e013add3b6cfc572e4c209babab75dcd
MisuStefanLeonard <aade211@yahoo.com>
2025-02-18 15:52:23 +0200

    Update migrations.sh

--- C# projects/E_Commerce_BackEnd/E_Commerce_BackEnd/migrations.sh
+++ C# projects/E_Commerce_BackEnd/E_Commerce_BackEnd/migrations.sh
@@ -33,7 +33,10 @@
 set -e  # Exit script on error
 
 echo "🚀 Running database migrations..."
+
+export PATH="$PATH:/root/.dotnet/tools"
 dotnet ef database update --no-build --project /app/E_Commerce_BackEnd.dll
+
 echo "✅ Migrations applied successfully!"
 
 echo "🚀 Running Quartz SQL script..."
