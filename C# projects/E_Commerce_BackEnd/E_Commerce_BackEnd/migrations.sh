3e6c8b59a904c7c72bb43b12ebff5d47580df657
MisuStefanLeonard <aade211@yahoo.com>
2025-02-18 14:00:55 +0200

    Update migrations.sh

--- C# projects/E_Commerce_BackEnd/E_Commerce_BackEnd/migrations.sh
+++ C# projects/E_Commerce_BackEnd/E_Commerce_BackEnd/migrations.sh
@@ -6,12 +6,12 @@ echo "Starting migrations script"
 export PATH="$PATH:/root/.dotnet/tools"
 
 # ✅ Check if migrations need to be applied
-PENDING_MIGRATIONS=$(dotnet ef migrations list | grep -v "No migrations applied" || true)
-
-if [[ -z "$PENDING_MIGRATIONS" ]]; then
-  echo "✅ Database is already up to date. Skipping migrations."
-  exit 0  # ✅ Exit successfully to mark container as completed
-fi
+#PENDING_MIGRATIONS=$(dotnet ef migrations list | grep -v "No migrations applied" || true)
+#
+#if [[ -z "$PENDING_MIGRATIONS" ]]; then
+#  echo "✅ Database is already up to date. Skipping migrations."
+#  exit 0  # ✅ Exit successfully to mark container as completed
+#fi
 
 echo "Running Quartz initialization script..."
 
